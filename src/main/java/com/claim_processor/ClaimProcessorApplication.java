96c915edfbcff59a7debfb4c4347bfcc3cf8ab47
Ben <benkeller08@gmail.com>
2025-07-19 19:48:53 -0500

    working docker/kubernetes builds

---
+++ src/main/java/com/claim_processor/ClaimProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.claim_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimProcessorApplication.class, args);
+	}
+
+}
